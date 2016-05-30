ed712063ed6a473465a414d5fd7761709ed41fca
hero.dot <kape.wolf@web.de>
2016-05-30 22:39:08 +0200

    Alles etwas kompakter

--- Task2.R
+++ Task2.R
@@ -2,6 +2,9 @@
 #a Basic plot structure (values colored by region)
 EconimistData <- read.csv("EconomistData.csv")
 library(ggplot2)
+install.packages("ggrepel")
+library(greppel)
+library(grid)
 
 
 pointsToLabel <- c("Afghanistan", "Greece", "China", "India", "Rwanda", "Spain", "France", "United States", "Japan", "Norway", "Singapore")
@@ -11,8 +14,24 @@ ggplot(EconimistData, aes(x = CPI, y = HDI, color = Region))+
   geom_point(size = 4.5, shape = 21) +
   geom_point(size = 4, shape = 21) +
   geom_point(size = 3.5, shape = 21)+
-  geom_text(aes(label = Country),  color = "gray20",
-            data = subset(EconimistData, Country %in% pointsToLabel))
+  geom_text_repel(aes(label = Country),color = "gray20",data = subset(EconimistData, Country %in% pointsToLabel),force = 10)+
+  
+  scale_x_continuous(name = "Corruption Perceptions Index, 2011 (10=least corrupt)",
+                     limits = c(.9, 10.5),
+                     breaks = 1:10) +
+  scale_y_continuous(name = "Human Development Index, 2011 (1=Best)",
+                     limits = c(0.2, 1.0),
+                     breaks = seq(0.2, 1.0, by = 0.1)) +
+  scale_color_manual(name = "",
+                     values = c("#24576D",
+                                "#099DD7",
+                                "#28AADC",
+                                "#248E84",
+                                "#F2583F",
+                                "#96503F")) +
+  ggtitle("Corruption and Human development")-> graph
+
+graph
 
 a1 <- ggplot(EconimistData, aes(x = CPI, y = HDI, color = Region))
 a1 + geom_point()
