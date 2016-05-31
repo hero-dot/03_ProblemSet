c18d0e8e84d10772ed16b1613ae936e53e8b8e95
hero.dot <kape.wolf@web.de>
2016-05-31 16:01:03 +0200

    deleting crap

--- Task3.R
+++ Task3.R
@@ -45,54 +45,6 @@ gapminderLocal%>%
   facet_wrap(~year)-> graph
 graph
 
-
-#b.
-
-graph3b <- function(x){
-    gapminderLocal%>%
-      filter(year==x)%>%
-      group_by(continent)%>%
-      ggplot(.,aes(x=gdpPercap,y=lifeExp,frame=year))+
-      geom_point(aes(color=continent,size=pop))+
-      scale_color_manual(values = c("blue","yellow","red","brown","green"))+
-      scale_size_continuous(range = c(2,20),limits = c(0,10e+07),breaks=seq(0,10e+07,10e+06))+
-      scale_x_log10(breaks=c(400,4000,40000))+
-      scale_y_continuous(limits = c(25,80), breaks = seq(25,80,25),labels = c("25   \nyears", "50   \nyears","75   \nyears"))+
-      xlab("income")+
-      ylab("lifespan")
-}
-
-
-for (year in unique(gapminderLocal$year)) {
-  graph3b(year)
-}
-
-lapply(unique(gapminderLocal$year), graph3b)
-
-years = 0
-for (years in unique(gapminderLocal$year)) {
-  gapminderLocal%>%
-    filter(year==years)%>%
-    group_by(continent)%>%
-    ggplot(.,aes(x=gdpPercap,y=lifeExp))+
-    geom_point(aes(color=continent,size=pop))+
-    scale_color_manual(values = c("blue","yellow","red","brown","green"))+
-    scale_size_continuous(range = c(2,20),limits = c(0,10e+07),breaks=seq(0,10e+07,10e+06))+
-    scale_x_log10(breaks=c(400,4000,40000))+
-    scale_y_continuous(limits = c(25,80), breaks = seq(25,80,25),labels = c("25   \nyears", "50   \nyears","75   \nyears"))+
-    xlab("income")+
-    ylab("lifespan")
-
-}
-
-graph <- ggplot(gapminderLocal, aes(gdpPercap, lifeExp, size=pop, color=continent,frame=year))+
-  geom_point()+scale_x_log10()
-graph
-install.packages("devtools")
-devtools::install_github("dgrtwo/gganimate")
-library(gganimate)
-gg_animate(graph)
-
 #c.
 
 graph <- ggplot(gapminderLocal, aes(gdpPercap, lifeExp, color=country))+
