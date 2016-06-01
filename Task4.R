16c8a68aea35e9d238b7d65a4fe85d5b9284cf70
hero.dot <kape.wolf@web.de>
2016-06-01 13:59:53 +0200

    some changes

--- Task4.R
+++ Task4.R
@@ -1,5 +1,6 @@
 PopAnual = read.csv('WPP2015_DB02_Populations_Annual.csv')
 library(dplyr,magrittr,ggplot2)
+library(reshape2)
 
 # 1.Graph
 # Regional % change, 2015-2050 forecast
@@ -11,8 +12,14 @@ PopAnual%>%
   filter(Time==2015|Time==2050)%>%
   filter(Location=="Africa"|Location=="Oceania"|Location=="Latin America and the Caribbean"|Location=="Northern America"|Location=="Europe"|Location=="Asia")%>%
   filter(VarID==2)%>%
-  mutate(growthRate=())-> finalData
+  group_by(Location)%>%
+  mutate(growthRate=round((diff(PopTotal)/PopTotal)*100),3)%>%
+  ungroup()%>%
+  filter(Time==2015)%>%
+  select(Location,growthRate)%>%
+  arrange(desc(growthRate))%>%
+  ggplot(.,aes(x=reorder(Location,growthRate),growthRate))+
+  geom_bar(stat="identity",width = 0.5)+
+  geom_text(aes(x=Location,y=growthRate,label=growthRate,hjust = -0.5))+
+  coord_flip()-> finalData
 finalData
-
-
-unique(PopAnual$Location)
