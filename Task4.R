438b43e8e885d528e238ad8e44a014c51a86b09b
hero.dot <kape.wolf@web.de>
2016-06-02 11:48:40 +0200

    final commit

--- Task4.R
+++ Task4.R
@@ -1,6 +1,9 @@
 PopAnual = read.csv('WPP2015_DB02_Populations_Annual.csv')
 library(dplyr,magrittr)
 library(ggplot2)
+library(grid)
+install.packages("gridExtra")
+library(gridExtra)
 # 1.Graph
 # Regional % change, 2015-2050 forecast
 # Regions Africa, Oceania, Latin America&Caribbean, North America, Asia, Europe
@@ -47,7 +50,7 @@ PopAnual%>%
   group_by(Time)%>%
   ggplot(.,aes(reorder(Time,-PopTotal),PopTotal,fill=Location))+
   geom_bar(stat="identity",position = "stack",width = 0.5)+
-  geom_text(aes(as.factor(Time),TotPop(.)*10e5,label=TotPop(.),hjust=-0))+
+  geom_text(aes(as.factor(Time),TotPop(.)*10e5,label=TotPop(.)))+
   coord_flip()+
   theme_minimal()+
   theme(axis.text.x = element_blank(),
@@ -60,17 +63,64 @@ PopAnual%>%
 graph2
 
 #3.Graph
+# Für das Einfärben der Balken mit den Farben der Kontinenten sind Daten mit einer Zuordnung der Länder
+# zum jeweiligen Kontinent nötig. Aus dem vorhandenen Datensatz lassen sich diese nicht ohne hohen Aufwand 
+# extrahieren. Um die Balken im Hintergrund zu schattieren wäre zudem ein Datensatz mit fake Werten nötig, die 
+# eine solche Darstellung ermöglichen.
 
 PopAnual%>%
   select(Time,PopTotal,VarID,Location,LocID)%>%
-  filter(Time==1950|Time==2015|Time==2050)%>%
+  filter(Time==1950)%>%
   filter(LocID<900)%>%
   filter(VarID==2)%>%
   group_by(Time)%>%
   arrange(desc(PopTotal))%>%
   top_n(12,PopTotal)%>%
-  ggplot(.,aes(Location,PopTotal))+
+  ggplot(.,aes(reorder(Location,PopTotal),PopTotal/10e5))+
   geom_bar(stat="identity")+
-  facet_grid(.~Time)+
-  coord_flip()->graph3
-graph3
+  geom_text(aes(label=Location,hjust= 0))+
+  scale_y_continuous(limits= c(0,2),breaks=seq(0,2,1))+
+  coord_flip()+
+  theme_minimal()+
+  theme(axis.text.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        plot.title = element_text(hjust=0))+
+  labs(title="Most populous countries, bn",x="",y="1950")->graph3.plot1
+
+PopAnual%>%
+  select(Time,PopTotal,VarID,Location,LocID)%>%
+  filter(Time==2015)%>%
+  filter(LocID<900)%>%
+  filter(VarID==2)%>%
+  group_by(Time)%>%
+  arrange(desc(PopTotal))%>%
+  top_n(12,PopTotal)%>%
+  ggplot(.,aes(reorder(Location,PopTotal),PopTotal/10e5))+
+  geom_bar(stat="identity")+
+  geom_text(aes(label=Location,hjust= 0))+
+  scale_y_continuous(limits= c(0,2),breaks=seq(0,2,1))+
+  coord_flip()+
+  theme_minimal()+
+  theme(axis.text.y = element_blank(),
+        panel.grid.major.y = element_blank())+
+  labs(title="",x="",y="2015")->graph3.plot2
+
+PopAnual%>%
+  select(Time,PopTotal,VarID,Location,LocID)%>%
+  filter(Time==2050)%>%
+  filter(LocID<900)%>%
+  filter(VarID==2)%>%
+  group_by(Time)%>%
+  arrange(desc(PopTotal))%>%
+  top_n(12,PopTotal)%>%
+  ggplot(.,aes(reorder(Location,PopTotal),PopTotal/10e5))+
+  geom_bar(stat="identity")+
+  geom_text(aes(label=Location,hjust= 0))+
+  scale_y_continuous(limits= c(0,2),breaks=seq(0,2,1))+
+  coord_flip()+
+  theme_minimal()+
+  theme(axis.text.y = element_blank(),
+        panel.grid.major.y = element_blank())+
+  labs(title="",x="",y="2050")->graph3.plot3
+
+grid.arrange(graph3.plot1,graph3.plot2,graph3.plot3,ncol=3,nrow=1)
